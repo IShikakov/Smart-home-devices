a05f458e3fc1097fa50df50631da7fe242781e55
Ilia Shikakov <ilia.shikakov@gmail.com>
2023-03-28 15:26:43 +0200

    Add deserialization naming for response enum classes

--- data/src/main/java/com/noveogroup/modulotech/data/network/response/device/DeviceResponseMode.kt
+++ data/src/main/java/com/noveogroup/modulotech/data/network/response/device/DeviceResponseMode.kt
@@ -1,7 +1,11 @@
 package com.noveogroup.modulotech.data.network.response.device
 
+import com.squareup.moshi.Json
+
 internal enum class DeviceResponseMode {
+    @Json(name = "ON")
     On,
+
+    @Json(name = "OFF")
     Off,
-    ;
 }
--- data/src/main/java/com/noveogroup/modulotech/data/network/response/device/DeviceResponseType.kt
+++ data/src/main/java/com/noveogroup/modulotech/data/network/response/device/DeviceResponseType.kt
@@ -1,8 +1,14 @@
 package com.noveogroup.modulotech.data.network.response.device
 
+import com.squareup.moshi.Json
+
 internal enum class DeviceResponseType {
+    @Json(name = "Light")
     Light,
+
+    @Json(name = "Heater")
     Heater,
+
+    @Json(name = "RollerShutter")
     RollerShutter,
-    ;
 }
