662f1b9b24b359d24a93425b1a24edfea9356307
Ilia Shikakov <ilia.shikakov@gmail.com>
2023-03-28 16:34:18 +0200

    Improve DateConverter

--- data/src/main/java/com/noveogroup/modulotech/data/database/converters/DateConverter.kt
+++ data/src/main/java/com/noveogroup/modulotech/data/database/converters/DateConverter.kt
@@ -1,14 +1,27 @@
 package com.noveogroup.modulotech.data.database.converters
 
 import androidx.room.TypeConverter
+import java.text.SimpleDateFormat
 import java.util.*
 
 internal object DateConverter {
-    @TypeConverter
-    @JvmStatic
-    fun dateToLong(date: Date?): Long? = date?.time
+
+    private const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ"
+    private const val UTC_TIME_ZONE = "UTC"
+    private val dateFormat by lazy {
+        SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH).apply {
+            timeZone = TimeZone.getTimeZone(UTC_TIME_ZONE)
+        }
+    }
 
     @TypeConverter
     @JvmStatic
-    fun longToDate(millis: Long?): Date? = millis?.let { Date(it) }
+    fun dateToLong(date: Date?): String? = date?.let {
+        println("dateToLong $date")
+        dateFormat.format(it)
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun longToDate(string: String?): Date? = string?.let { dateFormat.parse(it) }
 }
