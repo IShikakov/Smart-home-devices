3a6bc7d96936895a68bf7059f294a4a94b1d71a1
Ilia Shikakov <ilia.shikakov@gmail.com>
2023-03-28 18:27:54 +0200

    Fix status bar color depending on theme

--- app/src/main/java/com/noveogroup/modulotech/ui/theme/AppTheme.kt
+++ app/src/main/java/com/noveogroup/modulotech/ui/theme/AppTheme.kt
@@ -2,6 +2,7 @@ package com.noveogroup.modulotech.ui.theme
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun SmartHomeAppTheme(
@@ -9,6 +10,14 @@ fun SmartHomeAppTheme(
     content: @Composable () -> Unit,
 ) {
     val colorScheme = if (darkThemeEnabled) darkColorPalette else lightColorPalette
+    val systemUiController = rememberSystemUiController()
+    if (darkThemeEnabled) {
+        systemUiController.setStatusBarColor(color = colorScheme.background)
+        systemUiController.setNavigationBarColor(color = colorScheme.background)
+    } else {
+        systemUiController.setStatusBarColor(color = colorScheme.secondaryVariant)
+        systemUiController.setNavigationBarColor(color = colorScheme.background)
+    }
     MaterialTheme(
         colors = colorScheme,
         typography = smartHomeTypography,
