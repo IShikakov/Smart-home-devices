62b43fabe72110b44ad604729476e53929e342b7
Ilia Shikakov <ilia.shikakov@gmail.com>
2023-03-29 10:26:00 +0200

    Fix enum obfuscation with Moshi

--- data/src/main/java/com/noveogroup/modulotech/data/network/response/device/DeviceResponseMode.kt
+++ data/src/main/java/com/noveogroup/modulotech/data/network/response/device/DeviceResponseMode.kt
@@ -1,7 +1,9 @@
 package com.noveogroup.modulotech.data.network.response.device
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = false)
 internal enum class DeviceResponseMode {
     @Json(name = "ON")
     On,
--- data/src/main/java/com/noveogroup/modulotech/data/network/response/device/DeviceResponseType.kt
+++ data/src/main/java/com/noveogroup/modulotech/data/network/response/device/DeviceResponseType.kt
@@ -1,7 +1,9 @@
 package com.noveogroup.modulotech.data.network.response.device
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = false)
 internal enum class DeviceResponseType {
     @Json(name = "Light")
     Light,
